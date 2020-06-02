4fe4c8d54981e56101b573899d5aa713e97729d7
hasiburratul <hasibur.ratul@gmail.com>
2020-06-02 21:12:26 +0600

    Initial

---
+++ Chapter 2/Ex2_04.py
@@ -0,0 +1,11 @@
+width = 17
+height = 12.0
+x = width//2
+y = width/2.0
+z = height/3
+a = 1 + 2 *5
+
+print('\n',x)
+print('\n',y)
+print('\n',z)
+print('\n',a)
---
+++ Chapter 2/Ex2_05.py
@@ -0,0 +1,6 @@
+c = float(input('Enter Celcius Temperature:\n'))
+
+#Fahrenheit=(Celsius * 9/5) + 32
+f =  (c * 9/5) + 32
+
+print('Temperature in Fahrenheit:',f)
